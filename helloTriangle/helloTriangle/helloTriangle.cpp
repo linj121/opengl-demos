ca2849087081987c122b824f5d989ab8535700d8
linj121 <jiexulin99@gmail.com>
2024-09-22 03:24:27 -0400

    exercise(triangle): generalization for adding vertex data of N triangles

--- helloTriangle/helloTriangle/helloTriangle.cpp
+++ helloTriangle/helloTriangle/helloTriangle.cpp
@@ -2,10 +2,24 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
+struct Vertex {
+    float x;
+    float y;
+    float z;
+};
+
+struct Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+};
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // settings
+const short int NUMBER_OF_TRIANGLES = 3;
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
@@ -108,41 +122,43 @@ int main()
     //    0, 1, 3,  // first Triangle
     //    1, 2, 3   // second Triangle
     //};
-    float vertices[] = {
-        -1.0f, 0.0f,   0.0f,  // triangle 1 bottom left
-        -0.5f, 0.866f, 0.0f,  // triangle 1 top
-         0.0f, 0.0f,   0.0f,  // triangle 1 bottom right
-         0.0f, 0.0f,   0.0f,  // triangle 2 bottom left
-         0.5f, 0.866f, 0.0f,  // triangle 2 top
-         1.0f, 0.0f,   0.0f,  // triangle 2 bottom right
+
+    Triangle triangles[NUMBER_OF_TRIANGLES] = {
+        {
+            { -1.0f, 0.0f,   0.0f, }, // triangle 1 bottom left
+            { -0.5f, 0.866f, 0.0f, }, // triangle 1 top
+            {  0.0f, 0.0f,   0.0f, }, // triangle 1 bottom right
+        },
+        {
+            { 0.0f, 0.0f,   0.0f, }, // triangle 2 bottom left
+            { 0.5f, 0.866f, 0.0f, }, // triangle 2 top
+            { 1.0f, 0.0f,   0.0f, }, // triangle 2 bottom right
+        },
+        {
+            {  0.0f,  0.0f,   0.0f, },
+            { -0.5f, -0.866f, 0.0f, },
+            {  0.5f, -0.866f, 0.0f, },
+        }
     };
 
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    //glGenBuffers(1, &EBO); 
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    // Generate multiple VAOs and VBOs
+    unsigned int VAOs[NUMBER_OF_TRIANGLES], VBOs[NUMBER_OF_TRIANGLES];
+    glGenVertexArrays(NUMBER_OF_TRIANGLES, VAOs);
+    glGenBuffers(NUMBER_OF_TRIANGLES, VBOs);
+    
+    for (int i = 0; i < NUMBER_OF_TRIANGLES; i++)
+    {
+        glBindVertexArray(VAOs[i]);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOs[i]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(triangles[i]), &triangles[i], GL_STATIC_DRAW);
 
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
 
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -162,10 +178,15 @@ int main()
 
         // draw our first triangle
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        for (int i = 0; i < NUMBER_OF_TRIANGLES; i++)
+        {
+            glBindVertexArray(VAOs[i]);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glBindVertexArray(0);
+        }
+
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // no need to unbind it every time 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -175,8 +196,8 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, VAOs);
+    glDeleteBuffers(1, VBOs);
     //glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
@@ -202,4 +223,3 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
-
